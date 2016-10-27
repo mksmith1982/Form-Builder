216609a18dd3cccb4ebf4077618b4d5b8ec2bfdf
mksmith1982 <mksmith1982@gmail.com>
2016-10-27 08:29:07 -0400

    completed the majority of the script

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,67 @@
-import $ from 'jquery';
 
-var container = $("#formcontainer");
-console.log(container);
-
-
-function formTemplate(items){
-	return `
-	<li id="firstName">
-	 ${firstName.name} 
-
-	</li>;}`
-
-
-var userRequest = $.ajax({
-	url: `http://json-data.herokuapp.com/forms`
-});
+import $ from "jquery";
 
 
 
-// <form action="/my-handling-form-page" method="post">
-//     <div>
-//         <label for="name">Name:</label>
-//         <input type="text" id="name" name="user_name" />
-//     </div>
-//     <div>
-//         <label for="mail">E-mail:</label>
-//         <input type="email" id="mail" name="user_mail" />
-//     </div>
-//     <div>
-//         <label for="msg">Message:</label>
-//         <textarea id="msg" name="user_message"></textarea>
-//     </div>
-// </form>
+function getData(form_data, status , requestObject) {
+
+	// loop through all elements in json object
+	for (var i = 0; i < form_data.length; i++) {
+
+		// log the label for each form element
+		console.log	(form_data[i].label);
+
+		// log each form element
+		console.log(form_data[i]);
+
+		// if the type is select
+		if (form_data[i].type === "select") { 
+
+			var label = `<label>${form_data[i].label}:</label>`;
+			var icon  = `<i class = "fa ${form_data[i].icon}"> </i>`;
+			var select = $(`<select id = "${form_data[i].id}"></select>`);
+
+			// loop through each option of the select
+			for (var a = 0; a < form_data[i].options.length; a++) {
+
+				var language = `<option value= "${form_data[i].options[a].value}"> ${form_data[i].options[a].label}</option>`;
+				console.log(form_data[i].options[a]);
+				select.append(language);
+				
+			}
+
+			// <select name = "mySelect">
+			//    <option value= "option1"> English</option>
+			//    <option value= "option2"> Spanish</option>
+			//    <option value= "option3"> Japanese</option>
+			// </select>
+
+			// add the label, icon & select element
+			$("#myform").append(label);
+			$("#myform").append(icon);
+			$("#myform").append(select);
 
 
-// var firstName = $(#fistName);
-// var lastName  = $(#lastName);
-// var email= $(#email);
-// var currentWebsite = $(#currentWebsite);
-// var language = $(#language);
-// var comments = $(#comments);
-// var mobile = $(#mobile);
-// var home = $(#home);
-// var formSub = $(#formSub);
+			// end if type = select
+		} else {  
+			// else type != select
+			var label = `<label>${form_data[i].label}:</label>`;
+			var icon  = `<i class = "fa ${form_data[i].icon}"> </i>`;
+			var input = `<input type = "${form_data[i].type}" id = "${form_data[i].id}"/>`;
+
+			// add the label, icon & input element
+			$("#myform").append(label);
+			$("#myform").append(icon);
+			$("#myform").append(input);
+
+		}	// end else type != select
+
+	}	// end for i < form_data.length
+
+} // end getData
+
+
+
+$.getJSON("http://json-data.herokuapp.com/forms", {} , getData);
+
+
